c8d7a86f3cbb691cd1452292be409024ab45769c
Josias Werly <josias.werly@gmail.com>
2020-11-17 19:00:57 -0300

    First Simple Commit

--- src/Test/fnx.js
+++ src/Test/fnx.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import * as THREE from "three";
 
 class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.var = "myTest";
+        //this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        //this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        //this.cube = new THREE.Mesh( geometry, material );
+    }
     render() {
-        return (
-        <h1>{this.props.value}</h1>
-        )
+        return (<h1>test</h1>);
     }
 }
 export {Test};
--- src/index.js
+++ src/index.js
@@ -1,56 +1,57 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from "three";
-
+import { Test } from "./Test/fnx"
 
 
 class App extends Component {
   componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
+    this.h = <h1>f</h1>
   }
   render() {
-    return (
-      <div />
-    )
+    const out = [];
+    for (let index = 0; index < 10; index++) {
+      out.push(<Test/>)
+    }
+    return <div> {out} </div>
   }
 }
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 
 
+
+
 // import React, { Component } from "react";
 // import ReactDOM from "react-dom";
 // import * as THREE from "three";
-// import {Test} from "./Test/fnx"
-
-
 // class App extends Component {
+//   componentDidMount() {
+//     // === THREE.JS CODE START ===
+//     var scene = new THREE.Scene();
+//     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+//     var renderer = new THREE.WebGLRenderer();
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+//     document.body.appendChild( renderer.domElement );
+//     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//     var cube = new THREE.Mesh( geometry, material );
+//     scene.add( cube );
+//     camera.position.z = 5;
+//     var animate = function () {
+//       requestAnimationFrame( animate );
+//       cube.rotation.x += 0.01;
+//       cube.rotation.y += 0.01;
+//       renderer.render( scene, camera );
+//     };
+//     animate();
+//     // === THREE.JS EXAMPLE CODE END ===
+//   }
 //   render() {
-//     return <Test value={"Fun"}></Test>
+//     return (
+//       <div />
+//     )
 //   }
 // }
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
